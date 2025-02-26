916d73cce27fdce53db247a97257234856b4b128
Aryan Rathore <aryanrathoreop@gmail.com>
2025-02-26 14:17:26 +0530

    Create a zaprun table

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import express, {Request, Response} from "express"
+
+const prisma = new PrismaClient()
 const app = express()
 
 app.post("/hooks/catch/:userId/:zapId",(req:Request, res:Response)=>{
 const userId = req.params.userId;
 const zapId = req.params.zapId;
+prisma.z
 })
