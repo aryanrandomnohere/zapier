3a425381d59eb3ac3a8fe69b52b73f8824a88b0a
Aryan Rathore <aryanrathoreop@gmail.com>
2025-03-12 19:20:11 +0530

    Made some changes in signIn component

---
+++ Zapier/apps/frontend/app/components/SingIn.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState } from "react";
+import Input from "../components/Input";
+import { FcGoogle } from "react-icons/fc";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function SingIn() {
+    const [email,setEmail] = useState("")
+    const [firstname,setFirstname] = useState("")
+    const [lastname,setLastname] = useState("")
+  return (
+    <div className="max-w-[450px]  border border-black/10 p-6">
+    <div className="flex flex-col gap-4 max-w-[390px]"> <div className="flex items-center min-w-[390px] p-2 bg-blue-500/90 rounded hover:bg-blue-600 transition-all duration-500 cursor-pointer w-60 text-center justify-between">
+     <div className="bg-white p-0.5 rounded text-sm"><FcGoogle className="text-2xl" /></div>
+<div className="flex items-center gap-2 flex-grow justify-center">
+ <div className="text-sm font-extrabold text-white">Sign up with Google</div>
+</div>
+</div>
+<div className="flex items-center justify-center ">
+<div className="flex-grow border-t border-gray-300"></div>
+<span className="mx-2 text-gray-500 font-bold text-sm">OR</span>
+<div className="flex-grow border-t border-gray-300"></div>
+</div>
+<div className="text-sm">* Indicates a required field.</div>
+<Input value={email} onChange={setEmail} label="* Work email" placeholder=""/>
+<div className="flex">
+<Input value={firstname} onChange={setFirstname} label="* First name" placeholder=""/>
+<Input value={lastname} onChange={setLastname} label="* Last name" placeholder=""/>
+</div>
+<div className="text-sm font-medium">By signing up, you agree to Zapier's <a className="text-blue-700 underline" href="https://zapier.com/legal">terms of service and privacy policy.</a></div>
+<PrimaryButton size="small"> Get started for free</PrimaryButton>
+<div className="text-sm font-medium text-center">Already have an account? <a className="text-blue-700 underline">Log in</a></div>
+</div></div>
+  )
+}
--- Zapier/apps/frontend/app/sign-up/page.tsx
+++ Zapier/apps/frontend/app/sign-up/page.tsx
@@ -1,16 +1,11 @@
-"use client"
 import { TbCircleCheckFilled } from "react-icons/tb";
-import { FcGoogle } from "react-icons/fc";
-import Input from "../components/Input";
-import { useState } from "react";
-import PrimaryButton from "../components/PrimaryButton";
+import SingIn from "../components/SingIn";
+
 
 export default function page() {
-    const [email,setEmail] = useState("")
-    const [firstname,setFirstname] = useState("")
-    const [lastname,setLastname] = useState("")
+
   return (
-    <div className="flex justify-center items-center mt-[100px] gap-20 ">
+    <div className="flex flex-col"><div className="flex justify-center items-center mt-[50px] gap-20 ">
     <div className="flex flex-col  max-w-[400px] gap-5"> <div className="text-3xl font-bold leading-10">Join millions worldwide who automate their work using Zapier.</div>
     <div> <div className="flex gap-2 items-center text-sm font-medium">
         <TbCircleCheckFilled className="text-green-700 text-xl " /> Easy setup, no coding required</div>
@@ -19,30 +14,14 @@ export default function page() {
         <TbCircleCheckFilled className="text-green-700 text-xl " /> Free forever for core features</div>
         </div>
         <div> <div className="flex gap-2 items-center text-sm font-medium">
-        <TbCircleCheckFilled className="text-green-700 text-xl  " />14-day trial of premium features & apps</div>
+        <TbCircleCheckFilled className="text-green-700 text-xl " />14-day trial of premium features & apps</div>
         </div>
         </div>
-        <div className="flex flex-col gap-4 max-w-[450px]  border border-black/10 p-6">
-        <div className="flex items-center min-w-[390px] p-2 bg-blue-500/90 rounded hover:bg-blue-600 transition-all duration-500 cursor-pointer w-60 text-center justify-between">
-        <div className="bg-white p-0.5 rounded text-sm"><FcGoogle className="text-2xl" /></div>
-  <div className="flex items-center gap-2 flex-grow justify-center">
-    <div className="text-sm font-extrabold text-white">Sign up with Google</div>
-  </div>
-</div>
-<div className="flex items-center justify-center ">
-  <div className="flex-grow border-t border-gray-300"></div>
-  <span className="mx-2 text-gray-500 font-bold text-sm">OR</span>
-  <div className="flex-grow border-t border-gray-300"></div>
-</div>
-<div className="text-sm">* Indicates a required field.</div>
-<Input value={email} onChange={setEmail} label="* Work email" placeholder=""/>
-<Input value={firstname} onChange={setFirstname} label="* First name" placeholder=""/>
-<Input value={lastname} onChange={setLastname} label="* Last name" placeholder=""/>
-<div className="text-sm font-medium">By signing up, you agree to Zapier's <a className="text-blue-700 underline" href="https://zapier.com/legal">terms of service and privacy policy.</a></div>
-<PrimaryButton size="small"> Get started for free</PrimaryButton>
-<div className="text-sm font-medium text-center">Already have an account? <a className="text-blue-700 underline">Log in</a></div>
-</div>
-        
+        <div >
+       <SingIn/>
+       </div>
+        </div>
+<div className="text-lg text-center font-light mt-5">Trusted at companies large and small</div>
         </div>
   )
 }
