f14ff0c1adff45e2900bd097af255f9d5ecb1f5a
Aryan Rathore <aryanrathoreop@gmail.com>
2025-03-21 00:42:56 +0530

    Made beautiful ui

--- frontend/app/components/ZapCell.tsx
+++ frontend/app/components/ZapCell.tsx
@@ -1,6 +1,12 @@
 import { BiSolidZap } from "react-icons/bi";
-export default function ZapCell() {
+import { FiPlus } from "react-icons/fi";
+export default function ZapCell({title,subtitle,order}: {title: string, subtitle: string, order: number}) {
   return (
-    <div className=" flex pr-20 pl-2 shadow-xl min-w-[26rem] py-3 border border-black/10 "><div className="p-2.5 border border-black/20 rounded-md"><BiSolidZap className="text-white p-1.5 bg-black text-3xl rounded-full" /></div><div className=" flex flex-col pl-2"><div className="font-bold text-xl" >1. Trigger</div> <div className="">An event that starts your zap</div></div></div>
-  )
+    <div className="flex flex-col gap-0"><div className=" flex hover:cursor-pointer pr-20 pl-2 transition-all transform duration-200 hover:shadow-xl min-w-[26rem] py-2 border border-black/10 "><div className="p-2.5 border border-black/20 rounded-md"><BiSolidZap className="text-white p-1.5 bg-black text-3xl rounded-full" /></div><div className=" flex flex-col pl-2">
+      <div className="flex gap-1 font-bold text-xl">{order}. <div className="flex flex-col"><div className="font-bold text-xl" > {title}</div> <div className=" font-normal text-sm">{subtitle}</div></div></div></div></div>
+  <div className="flex flex-col gap-2 items-center justify-center h-25">
+      <div className="w-[0.5px] h-10 bg-gray-400"></div>
+      <FiPlus className="text-blue-600 text-xl hover:cursor-pointer" />
+      <div className="w-[0.5px] h-5 bg-gray-400"></div>
+    </div></div>)
 }
--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -2,8 +2,9 @@ import ZapCell from "@/app/components/ZapCell";
 
 export default function page() {
   return (
-    <div className="flex justify-center items-center min-w-screen h-screen ">
-    <ZapCell/>
+    <div className="flex flex-col justify-center items-center min-w-screen h-screen ">
+    <ZapCell title="Trigger" subtitle="An event that starts your Zap" order={1}  />
+    <ZapCell title="Action" subtitle="An event a Zap performs after it starts" order={2} />
         </div>
   )
 }
