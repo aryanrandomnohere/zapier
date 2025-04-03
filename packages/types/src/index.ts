0545b6d66e0bcd17ae1ca04535b07687652cebe5
Aryan Rathore <aryanrathoreop@gmail.com>
2025-04-03 20:45:12 +0530

    Created a common module to share types between packages

---
+++ packages/types/src/index.ts
@@ -0,0 +1,69 @@
+export interface zapInterface {
+  id: string;
+  name: string;
+  triggerId: string;
+  lastEdited: string;
+  userId: number;
+  actions: {
+    id: string;
+    zapId: string;
+    actionId: string;
+    sortingOrder: number;
+    actionDetails: {
+      id: string;
+      imagePath: string;
+      name: string;
+    };
+  }[];
+  trigger: {
+    id: string;
+    zapId: string;
+    triggerId: string;
+    type: {
+      id: string;
+      name: string;
+      imagePath: string;
+    };
+  };
+}
+
+
+export interface ItemType {
+  id: string;
+  name: string;
+  imagePath: string;
+  metadata?: itemStepMetaData[];
+}
+
+export interface ZapCreateType {
+  triggerId: string;
+  triggerMetadata: JSON;
+  actions: {
+    actionId: string;
+    actionMetadata: JSON;
+  }[];
+}
+
+export interface selectedItemMetaDataType {
+  index:number | null | undefined,
+  isOpen:boolean
+}
+
+export interface itemStepMetaData {
+stepName:string,
+stepNumber:number,
+stepDescription?:string,
+completed:boolean | null,
+fields:{
+  name:string,
+  fieldInputType:string,
+  fieldLabel:string,
+  fieldPlaceholder:string,
+  fieldValue:string,
+  required:boolean,
+  options?:string[]
+}[]
+}
+
+
+
