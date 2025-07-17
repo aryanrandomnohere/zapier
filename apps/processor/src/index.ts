9b221c85610d7e77ffe96950fa00932dc6b64b0a
Aryan Rathore <aryanrathoreop@gmail.com>
2025-07-17 19:59:47 +0530

    feat: added actual polling logic to processor (runs every 15 minutes)

--- apps/processor/src/index.ts
+++ apps/processor/src/index.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@repo/db";
 import { Kafka } from "kafkajs";
+import { poll } from "./poll.js";
 const kafka = new Kafka({
   clientId: "outbox-processor",
   brokers: ["localhost:9092"],
@@ -9,26 +10,82 @@ async function main() {
   const producer = kafka.producer();
   producer.connect();
   while (1) {
+    const pollingTriggers = await prisma.trigger.findMany({
+      where: {
+        published: true,
+        optionType: "polling",
+        OR: [
+          { lastPolledAt: null },
+          {
+            lastPolledAt: {
+              lt: new Date(Date.now() - 1 * 60 * 1000),
+            },
+          },
+        ],
+      },
+      take: 10,
+      include: {
+        userConnection: true,
+        type: true,
+      },
+    });
+    if (pollingTriggers.length != 0) console.log("1 trigger is present");
+    for (const trigger of pollingTriggers) {
+      try {
+        const record = await poll(trigger);
+        if (record === null) {
+          console.log("No new record polled");
+          continue;
+        }
+        // console.log(record)
+        const zapRun = await prisma.zapRun.create({
+          data: {
+            zapId: trigger.zapId,
+            metaData: record,
+          },
+        });
+        await producer.send({
+          topic: "zapier-events",
+          messages: [
+            {
+              value: JSON.stringify({
+                zapRunId: zapRun.id,
+                stage: 1,
+                payload: record,
+              }),
+            },
+          ],
+        });
+        await prisma.trigger.update({
+          where: { id: trigger.id },
+          data: { lastPolledAt: new Date() },
+        });
+      } catch (err) {
+        console.error("Error polling trigger", trigger.id, err);
+      }
+    }
     const pendingRows = await prisma.zapRunOutbox.findMany({
       take: 10,
     });
-    const produced = await producer.send({
-      topic: "zapier-events",
-      messages: pendingRows.map((r: any) => {
-        return {
-          value: JSON.stringify({ zapRunId: r.zapRunId, stage: 1 }),
-        };
-      }),
-    });
-    if (produced.length > 0) console.log("Added to kafka", produced);
-    const deletedZapRun = await prisma.zapRunOutbox.deleteMany({
-      where: {
-        id: {
-          in: pendingRows.map((r) => r.id),
+    if (pendingRows.length > 10) {
+      const produced = await producer.send({
+        topic: "zapier-events",
+        messages: pendingRows.map((r: any) => {
+          return {
+            value: JSON.stringify({ zapRunId: r.zapRunId, stage: 1 }),
+          };
+        }),
+      });
+      if (produced.length > 0) console.log("Added to kafka", produced);
+      const deletedZapRun = await prisma.zapRunOutbox.deleteMany({
+        where: {
+          id: {
+            in: pendingRows.map((r) => r.id),
+          },
         },
-      },
-    });
-    if (deletedZapRun.count > 0) console.log("Deleted", deletedZapRun);
+      });
+      if (deletedZapRun.count > 0) console.log("Deleted", deletedZapRun);
+    }
   }
 }
 main();
