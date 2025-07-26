d2c8a2e5afcee00b3db48d6db26dbcb03d109166
Aryan Rathore <aryanrathoreop@gmail.com>
2025-07-26 23:46:17 +0530

    feat(frontend): added scoped RecoilProvider to support client-only state while keeping other components server-rendered

---
+++ apps/frontend/app/dashboard/RecoilProvider.tsx
@@ -0,0 +1,7 @@
+import RecoilContextProvider from "../RecoilState/RecoilContextProvider";
+
+export default function RecoilProvider({children}:{children:React.ReactNode}) {
+  return (
+    <RecoilContextProvider>{children}</RecoilContextProvider>
+)
+}
