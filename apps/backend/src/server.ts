f9a19db2e1e6a6747b10f2056d4e6fdcbdcef9ff
Aryan Rathore <aryanrathoreop@gmail.com>
2025-08-07 21:15:14 +0530

    feat(backend): register folderRouter, apply auth middleware, and tweak CORS config

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,20 +5,31 @@ import cors from "cors";
 import actionRouter from "./router/actionRouter.js";
 import triggerRouter from "./router/triggerRouter.js";
 import cookieParser from "cookie-parser";
-import { AuthMiddleware } from "./Middleware/AuthMiddleware.js";
+import authMiddleware from "./Middleware/authMiddleware.js";
 import zapRunRouter from "./router/zapRunRouter.js";
+import errorMiddleware from "./Middleware/errorMIddleware.js";
+import { authRouter } from "./router/authRouter.js";
+import { folderRouter } from "./router/folderRoutes.js";
+
 const PORT = 3001;
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/v1/zap", zapRouter);
+app.use("/api/v1/zap", authMiddleware, zapRouter);
 app.use("/api/v1", userRouter);
-app.use("/api/v1/actions", actionRouter);
-app.use("/api/v1/triggers", triggerRouter);
-app.use("/api/v1/zap-runs", zapRunRouter);
-
+app.use("/api/v1/actions", authMiddleware, actionRouter);
+app.use("/api/v1/triggers", authMiddleware, triggerRouter);
+app.use("/api/v1/zap-runs", authMiddleware, zapRunRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/folders", authMiddleware, folderRouter);
+app.use(errorMiddleware);
 app.listen(PORT, () => {
   console.log("Server is listening on port: ", PORT);
 });
