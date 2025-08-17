c8861a30f990450fdb6df2404204150856af18b4
Aryan Rathore <aryanrathoreop@gmail.com>
2025-08-18 00:11:01 +0530

    Update CurrentOperation.tsx

--- apps/frontend/app/zap/create/[zapId]/CurrentOperation.tsx
+++ apps/frontend/app/zap/create/[zapId]/CurrentOperation.tsx
@@ -6,25 +6,29 @@ import ZapHistory from "./ZapHistory";
 import AdvancedSettings from "./AdvancedSettings";
 import ZapDetails from "./ZapDetails";
 
-export default function CurrentOperation({ operation }: { operation: zapOperations }) {
-    switch (operation) {
-      case zapOperations.LINKEDASSETS:
-        return <LinkedAssets />;
-      case zapOperations.ADVANCEDSETTINGS:
-        return <AdvancedSettings />;
-      case zapOperations.NOTES:
-        return <ZapNotes />;
-      case zapOperations.STATUS:
-        return <div>Status</div>;
-      case zapOperations.CHANGEHISTORY:
-        return <ZapHistory />;
-      case zapOperations.ZAPDETAILS:
-        return <ZapDetails />;
-      // case zapOperations.VERSIONS:
-      //   return <div>Versions</div>;
-      case zapOperations.ZAPRUN:
-        return <ZapRunDetails />;
-      default:
-        <>Error</>;
-    }
+export default function CurrentOperation({
+  operation,
+}: {
+  operation: zapOperations;
+}) {
+  switch (operation) {
+    case zapOperations.LINKEDASSETS:
+      return <LinkedAssets />;
+    case zapOperations.ADVANCEDSETTINGS:
+      return <AdvancedSettings />;
+    case zapOperations.NOTES:
+      return <ZapNotes />;
+    case zapOperations.STATUS:
+      return <div>Status</div>;
+    case zapOperations.CHANGEHISTORY:
+      return <ZapHistory />;
+    case zapOperations.ZAPDETAILS:
+      return <ZapDetails />;
+    // case zapOperations.VERSIONS:
+    //   return <div>Versions</div>;
+    case zapOperations.ZAPRUN:
+      return <ZapRunDetails />;
+    default:
+      <>Error</>;
   }
+}
