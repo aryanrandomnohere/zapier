816a61880268f01a02d04a1f74df67c4e4a02c29
Aryan Rathore <aryanrathoreop@gmail.com>
2025-08-24 23:59:01 +0530

    refactor: remove old component structure and move to app directory

--- apps/frontend/RecoilState/store/userAtom.ts
+++
@@ -1,8 +0,0 @@
-// recoil/userAtom.ts
-import { SessionType } from "@repo/types";
-import { atom } from "recoil";
-
-export const userAtom = atom<SessionType | undefined>({
-  key: "UserAtom",
-  default: undefined,
-});
